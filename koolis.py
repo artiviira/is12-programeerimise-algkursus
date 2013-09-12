9c4cd82badbf31952f96541076b3faaa0d79e9f4
opilane <opilane@PC215-10.(none)>
2013-09-12 09:54:00 +0300

    koolis

---
+++ koolis.py
@@ -0,0 +1,31 @@
+print "spam eggs"
+print "doesn\'t"
+print "doesn't"
+print "'jah,' ta ytles"
+print "\"jah,\" ta ytles"
+print '"Isn\'t,"she said'
+
+print "see on pikem l6ik ja et teha uut rida peab tegema .\.n k2su ilma punkti-\ndeta sest muidu ei t66ta see"
+print """
+K2sk: git[voimalused]
+	push		Uploadib stuffi
+	pull		Downloadib stuffi
+"""
+
+
+
+print r"See on pikk l6ik aga \n et anna uur rida sest jutum2rkide ees on t2ht r ehk raw"
+s = "Tuhat"
+print ("qwe"+""+s+""+"qwe")
+print "-" +s*5 + "-"
+print 'str' 'ing'
+print 'str'.strip() +'ing'
+
+s="asfasfasfasfasfasfasfasfasfasf"
+print s[3]
+print s[2:6]
+
+
+	
+
+
